e5bf6156a4a83c8ce78b4c91cfb8336cab9b0d8c
ApemasAlguem007 <geovanna.psilva2005@gmail>
2025-04-24 12:11:44 -0300

    Menu-Footer Template

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include 'parciais/header.php'; ?>
+
+<main>
+    <h1>Boas Vindas ao Site!</h1>
+    <p>Esse é um conteúdo de exemplo.</p>
+
+</main>
+
+<?php include 'parciais/footer.php'; ?>
---
+++ parciais/footer.php
@@ -0,0 +1,39 @@
+<footer class="site-footer">
+    <div class="footer-container">
+        <div class="footer-logo">
+            <a href="/menuefooter-parciais/"> <i class="fas fa-gem"> </i> Minha Marca </a>
+        </div>
+
+        <div class="footer-links">
+            <a href="/menuefooter-parciais/sobre.php"> <i class="fas fa-info-circle"></i> Sobre </a>
+
+            <a href="/menuefooter-parciais/servicos.php"> <i class="fas fa-concierge-bell"></i> Serviços </a>
+
+            <a href="/menuefooter-parciais/contato.php"> <i class="fas fa-envelope"></i> Contato </a>
+
+        </div>
+
+
+        <div class="footer-social">
+
+            <a href="https://facebook.com" target="_blank" arial-label="Facebook"> <i class="fab fa-facebook-f"></i>
+            </a>
+
+            <a href="https://instagram.com" target="_blank" arial-label="Instagram"> <i class="fab fa-instagram"></i>
+            </a>
+
+            <a href="https://linkedin.com" target="_blank" arial-label="Linkedin"> <i class="fab fa-linkedin"> </i>
+            </a>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+
+        <p>&copy; <?php echo date("Y"); ?> Minha Marca. Todos os direitos reservados. </p>
+
+    </div>
+
+</footer>
+</body>
+
+</html>
